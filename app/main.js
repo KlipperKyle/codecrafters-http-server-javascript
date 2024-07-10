683fc2c7957649878cfbf35f0219a849cb32fd44
Kyle Terrien <kyle@terren.us>
2024-07-10 16:20:16 -0700

    Add HTTP 200 response

--- app/main.js
+++ app/main.js
@@ -1,10 +1,13 @@
+#!/usr/bin/env node
 const net = require("net");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+    socket.on("close", () => {
+	socket.end();
+    });
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
     socket.end();
-  });
 });
 
 server.listen(4221, "localhost");
