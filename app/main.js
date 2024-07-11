49ee0d2a44c32405d207b7b3165da44f893a51af
Kyle Terrien <kyle@terren.us>
2024-07-11 16:05:16 -0700

    More refactor, add /user-agent endpoint

--- app/main.js
+++ app/main.js
@@ -4,10 +4,10 @@ const net = require("net");
 
 function dispatchClient(socket) {
     let buffer = "";
-    socket.on("close", function() {
-	socket.end();
-    });
-    socket.on("data", function(data) {
+    let response = "";
+    let headers = {};
+    let userAgentEcho = false;
+    function getData(data) {
 	let newdata = data.toString();
 	// You should buffer input.
 	// https://www.reddit.com/r/node/comments/59zgte/comment/d9cnymh/
@@ -17,31 +17,57 @@ function dispatchClient(socket) {
 	lines = lines.slice(0, -1);
 	lines.forEach(function(line) {
 	    let matches;
-	    // GET request
 	    if (matches = /^GET\s+(\S+)/.exec(line)) {
+		// GET request
 		let path = matches[1];
 		if (path === "/") {
+		    // GET /
 		    console.log(`200 OK: GET ${path} from ` +
 				`${socket.remoteAddress}:${socket.remotePort}`);
-		    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+		    response = "HTTP/1.1 200 OK\r\n\r\n";
 		} else if (matches = /^\/echo\/(.*)/.exec(path)) {
+		    // GET /echo/{str}
 		    console.log(`200 OK: GET ${path} from ` +
 				`${socket.remoteAddress}:${socket.remotePort}`);
 		    let str = matches[1];
-		    let response = "HTTP/1.1 200 OK\r\n";
+		    response = "HTTP/1.1 200 OK\r\n";
 		    response += "Content-Type: text/plain\r\n";
 		    response += `Content-Length: ${str.length}\r\n\r\n`;
 		    response += str;
-		    socket.write(response);
+		} else if (matches = /^\/user-agent\/?$/.exec(path)) {
+		    // GET /user-agent
+		    // Echo the User-Agent later
+		    userAgentEcho = true;
 		} else {
+		    // GET {anything else}
 		    console.error(`ERROR: 404 Not Found: GET ${path} from ` +
 				  `${socket.remoteAddress}:${socket.remotePort}`);
-		    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+		    response = "HTTP/1.1 404 Not Found\r\n\r\n";
 		}
-		socket.end();
+	    } else if (matches = /^(\S+):\s*(.*)/.exec(line)) {
+		// Header: Value
+		let header = matches[1];
+		let value = matches[2];
+		headers[header] = value;
+	    } else if (matches = /^$/.exec(line)) {
+		// That's the whole request, folks!
+		if (userAgentEcho) {
+		    let ua = headers["User-Agent"] ?? "";
+		    response = "HTTP/1.1 200 OK\r\n";
+		    response += "Content-Type: text/plain\r\n";
+		    response += `Content-Length: ${ua.length}\r\n\r\n`;
+		    response += ua;
+		}
+		close();
 	    }
 	});
-    });
+    }
+    function close() {
+	socket.write(response);
+	socket.end();
+    }
+    socket.on("data", getData);
+    socket.on("close", close);
 }
 
 const server = net.createServer(dispatchClient);
