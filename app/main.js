b4de45687fa8549b37b62eed11a5ae90138e0776
Kyle Terrien <kyle@terren.us>
2024-07-12 15:48:17 -0700

    Peel closure off into a readLine function

--- app/main.js
+++ app/main.js
@@ -41,6 +41,7 @@ function parseArgs(argv) {
 }
 
 function dispatchClient(socket) {
+    // Handle a single client connection
     let buffer = "";
     let response = "";
     let headers = {};
@@ -53,54 +54,55 @@ function dispatchClient(socket) {
 	let lines = buffer.split("\r\n");
 	buffer = lines[lines.length - 1];
 	lines = lines.slice(0, -1);
-	lines.forEach(function(line) {
-	    let matches;
-	    if (matches = /^GET\s+(\S+)/.exec(line)) {
-		// GET request
-		let path = matches[1];
-		if (path === "/") {
-		    // GET /
-		    console.log(`200 OK: GET ${path} from ` +
-				`${socket.remoteAddress}:${socket.remotePort}`);
-		    response = "HTTP/1.1 200 OK\r\n\r\n";
-		} else if (matches = /^\/echo\/(.*)/.exec(path)) {
-		    // GET /echo/{str}
-		    console.log(`200 OK: GET ${path} from ` +
-				`${socket.remoteAddress}:${socket.remotePort}`);
-		    let str = matches[1];
-		    response = "HTTP/1.1 200 OK\r\n";
-		    response += "Content-Type: text/plain\r\n";
-		    response += `Content-Length: ${str.length}\r\n\r\n`;
-		    response += str;
-		} else if (matches = /^\/user-agent\/?$/.exec(path)) {
-		    // GET /user-agent
-		    // Echo the User-Agent later
-		    console.log(`200 OK: GET ${path} from ` +
-				`${socket.remoteAddress}:${socket.remotePort}`);
-		    userAgentEcho = true;
-		} else {
-		    // GET {anything else}
-		    console.error(`ERROR: 404 Not Found: GET ${path} from ` +
-				  `${socket.remoteAddress}:${socket.remotePort}`);
-		    response = "HTTP/1.1 404 Not Found\r\n\r\n";
-		}
-	    } else if (matches = /^(\S+):\s*(.*)/.exec(line)) {
-		// Header: Value
-		let header = matches[1].toLowerCase();
-		let value = matches[2];
-		headers[header] = value;
-	    } else if (matches = /^$/.exec(line)) {
-		// That's the whole request, folks!
-		if (userAgentEcho) {
-		    let ua = headers["user-agent"] ?? "";
-		    response = "HTTP/1.1 200 OK\r\n";
-		    response += "Content-Type: text/plain\r\n";
-		    response += `Content-Length: ${ua.length}\r\n\r\n`;
-		    response += ua;
-		}
-		close();
+	lines.forEach(readLine);
+    }
+    function readLine(line) {
+	let matches;
+	if (matches = /^GET\s+(\S+)/.exec(line)) {
+	    // GET request
+	    let path = matches[1];
+	    if (path === "/") {
+		// GET /
+		console.log(`200 OK: GET ${path} from ` +
+			    `${socket.remoteAddress}:${socket.remotePort}`);
+		response = "HTTP/1.1 200 OK\r\n\r\n";
+	    } else if (matches = /^\/echo\/(.*)/.exec(path)) {
+		// GET /echo/{str}
+		console.log(`200 OK: GET ${path} from ` +
+			    `${socket.remoteAddress}:${socket.remotePort}`);
+		let str = matches[1];
+		response = "HTTP/1.1 200 OK\r\n";
+		response += "Content-Type: text/plain\r\n";
+		response += `Content-Length: ${str.length}\r\n\r\n`;
+		response += str;
+	    } else if (matches = /^\/user-agent\/?$/.exec(path)) {
+		// GET /user-agent
+		// Echo the User-Agent later
+		console.log(`200 OK: GET ${path} from ` +
+			    `${socket.remoteAddress}:${socket.remotePort}`);
+		userAgentEcho = true;
+	    } else {
+		// GET {anything else}
+		console.error(`ERROR: 404 Not Found: GET ${path} from ` +
+			      `${socket.remoteAddress}:${socket.remotePort}`);
+		response = "HTTP/1.1 404 Not Found\r\n\r\n";
 	    }
-	});
+	} else if (matches = /^(\S+):\s*(.*)/.exec(line)) {
+	    // Header: Value
+	    let header = matches[1].toLowerCase();
+	    let value = matches[2];
+	    headers[header] = value;
+	} else if (matches = /^$/.exec(line)) {
+	    // That's the whole request, folks!
+	    if (userAgentEcho) {
+		let ua = headers["user-agent"] ?? "";
+		response = "HTTP/1.1 200 OK\r\n";
+		response += "Content-Type: text/plain\r\n";
+		response += `Content-Length: ${ua.length}\r\n\r\n`;
+		response += ua;
+	    }
+	    close();
+	}
     }
     function close() {
 	socket.write(response);
